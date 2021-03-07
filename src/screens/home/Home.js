a7c0c23617f2228a590a3fbbd367e1e3b581f680
Arun Subash Manickam <arunsubashm@gmail.com>
2021-03-07 15:48:44 +0530

    Add Router NPM Packagae Add a controller to navigate different pages Implement the initial version of the Home page

---
+++ src/screens/home/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+
+    render () {
+        return (
+            <div>
+                <div>
+                    <header className="app-header">
+                        <div className="app-logo">
+                        <   span>Image Viewer</span>
+                        </div>
+                    </header>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default (Home);
